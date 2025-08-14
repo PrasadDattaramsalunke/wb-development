b2039a2c76025dcdd77abfded5b35cea318873db
PrasadDattaramsalunke <salunkeprasad922@gmail.com>
2025-08-14 18:11:52 +0000

    Added all required features to the website

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app=Flask(__name__)
-@app.route('/')
+JOBS=[
+    {
+        'id':1,
+        'Project':'Data Analyst',
+        'Place':'Bengaluru,India',
+        'Skill set':'Rs.10,00,000'
+    },
+    {
+        'id':2,
+        'Project':'Data Scientist',
+        'Place':'Delhi,India',
+        'Skill set':'Rs.15,00,000'
+    }
+]
+@app.route("/")
 def hello_world():
-    return 'Hello World'
+    return render_template('home.html',jobs=JOBS)
   
 if __name__=='__main__':
   app.run(debug=True,host='0.0.0.0')
